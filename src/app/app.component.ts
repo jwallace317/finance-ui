a643677930f8d0984eeb2716f87b8e7e6a06b2db
James  Wallace <jameswallace@Jamess-MBP.lan1>
2019-11-20 21:55:45 -0500

    created Stonks header and added in high low stocks graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,6 +78,7 @@ export class AppComponent {
 
         this.chart = chart;
     });
+    
   }
 
   ngOnDestroy() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
